3efc3d40ad5ab582c247b1012770e68561b6dae6
hxAri <90847846+hxAri@users.noreply.github.com>
2022-04-24 18:09:33 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# @author hxAri
+# @create 24.04-2022
+# @update -
+# @github https://github.com/hxAri
+#
+# Copyright (c) 2022 hxAri <ari160824@gmail.com>
+#
+# GNU General Public License v3, 29 June 2007
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+#
+
+# Script Version
+version="v1.0.0"
+
+errors=()
+
+# Clear Screen
+clear
